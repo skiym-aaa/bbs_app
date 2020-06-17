11413f29e6e8961d237e20e3d55af28255e46726
skiym-aaa <y.sakiyama3216@gmail.com>
2020-06-17 14:18:53 +0000

    [Add] ユーザー詳細,編集ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :baria_user, except: %i[show]
+  before_action :set_user
+
   def show
   end
 
@@ -6,5 +9,24 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: '編集が完了しました！'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :reset_password_token, :name, :introduction, :image)
+  end
+
+  def baria_user
+    redirect_to root_path unless params[:id].to_i == current_user.id
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '61299f413fb17dc7b949285e2a5f8b6378768cd6573d525104fde406083f9c8bd7f11f1c404a26c7e0fc9ac36090134d9232492b66c1bb4a2a29eb551e9519ae'
