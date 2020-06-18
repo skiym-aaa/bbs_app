21e64e3a7e608abe8334a13fbfbde56ac39a67c8
skiym-aaa <y.sakiyama3216@gmail.com>
2020-06-18 04:35:56 +0000

    [Add] 検索機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @q = Post.ransack(params[:q])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @posts = Post.all
+    @posts = @q.result
   end
 
   def show
